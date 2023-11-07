455fdb310ba1e4c7d8cc88450e5d90a49bb29186
sarathpk931 <sarathpk931@gmail.com>
2023-11-07 13:10:42 +0530

    fts

--- webpack.custom.js
+++ webpack.custom.js
@@ -1,21 +1,3 @@
-// webpack.custom.js
-const path = require('path');
-
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(ts)$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          },
-          'ts-loader'
-        ]
-      }
-    ]
-  }
+  //entry: ["@babel/polyfill", "./src/main.ts"],
 };
